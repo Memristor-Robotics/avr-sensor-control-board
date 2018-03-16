ed3da947448440b472b89b3a0a8f060099f42c5b
nikirasevic <rasevicnikola@yahoo.com>
2018-03-16 03:45:29 +0100

    Refactored

---
+++ .dep/BinarySensor.o.d
@@ -0,0 +1,10 @@
+build/./src/BinarySensor.o: src/BinarySensor.c src/BinarySensor.h \
+ src/Config.h src/Pin.h src/Utils.h
+
+src/BinarySensor.h:
+
+src/Config.h:
+
+src/Pin.h:
+
+src/Utils.h:
---
+++ .dep/uart0.o.d
@@ -0,0 +1,3 @@
+build/./src/uart0.o: src/uart0.c src/uart0.h
+
+src/uart0.h:
